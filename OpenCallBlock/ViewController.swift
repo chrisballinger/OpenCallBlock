ee1e42157b6337e71c746e2587494abb62eaa108
Chris Ballinger <chrisballinger@gmail.com>
2017-10-27 12:20:38 -0700

    Initial Commit

---
+++ OpenCallBlock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenCallBlock
+//
+//  Created by Chris Ballinger on 10/27/17.
+//  Copyright © 2017 Chris Ballinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
